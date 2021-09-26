f45fa673435218dab612caf8ea47fa5be331d76a
Peter Ihimire <peterihimire@gmail.com>
2021-09-26 13:34:16 +0100

    Redirected / to /movies so I could get rid of the homepage entirely in the application, which was pretty redundant

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import "./App.css";
-import HomePage from "./pages/HomePage";
+// import HomePage from "./pages/HomePage";
 import SearchPage from "./pages/SearchPage";
 import MovieDetails from "./pages/MovieDetails";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/" exact component={HomePage} />
+          {/* <Route path="/" exact component={HomePage} /> */}
           <Route path="/movies" component={SearchPage} />
           <Route path="/movie-details/:movieId" component={MovieDetails} />
+          <Redirect from="/" to="/movies" exact />
         </Switch>
       </Router>
     </div>
