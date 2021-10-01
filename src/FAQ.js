71a8f42ddc60842c48ea1260e7ca3b6f45e7ae0c
Peter Ihimire <peterihimire@gmail.com>
2021-10-01 23:31:55 +0100

    Added some external stuff not need like the faq

---
+++ src/FAQ.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
+const Question = ({ title, info }) => {
+  const [expanded, setExpanded] = useState(false)
+
+  return (
+    <article className='question'>
+      <header>
+        <h4 onClick={() => setExpanded(!expanded)} className='question-title'>
+          {title}
+        </h4>
+        <button className='btn' onClick={() => setExpanded(!expanded)}>
+          {expanded ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {expanded && <p>{info}</p>}
+    </article>
+  )
+}
+
+export default Question
+
+import React, { useState } from 'react'
+import data from './data'
+import SingleQuestion from './Question'
+
+const App = () => {
+  const [questions, setQuestions] = useState(data)
+
+  return (
+    <main>
+      <div className='container'>
+        <h3>questions and answers about login</h3>
+        <section className='info'>
+          {questions.map((question) => (
+            <SingleQuestion key={question.id} {...question} />
+          ))}
+        </section>
+      </div>
+    </main>
+  )
+}
+
+export default App
+
+
+const questions = [
+  {
+    id: 1,
+    title: 'Do I have to allow the use of cookies?',
+    info:
+      'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
+  },
+  {
+    id: 2,
+    title: 'How do I change my My Page password?',
+    info:
+      'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
+  },
+  {
+    id: 3,
+    title: 'What is BankID?',
+    info:
+      'Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
+  },
+  {
+    id: 4,
+    title: 'Whose birth number can I use?',
+    info:
+      'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
+  },
+  {
+    id: 5,
+    title: 'When do I recieve a password ordered by letter?',
+    info:
+      'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
+  },
+]
+export default questions
