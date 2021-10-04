c21abebdcb771ed08a4b51714a8dc1255c9451a8
Peter Ihimire <peterihimire@gmail.com>
2021-10-04 23:48:12 +0100

    Added backdrop and sideDrawer tsx files for later reference for a project and more in app.css file

---
+++ src/components/Backdrop.tsx
@@ -0,0 +1,25 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// // import Typography from "./../Typography/Typography";
+
+// interface BackdropProps {
+//   text?: any;
+//   type?: string;
+//   cssClass?: string;
+// }
+// const Backdrop: React.FC<BackdropProps> = (props) => {
+//   // return (
+//   //   // <Typography
+//   //   //   type="span"
+//   //   //   text={text}
+//   //   //   cssClass={`badge ${type} ${cssClass}`}
+//   //   // />
+
+//   // );
+//   return ReactDOM.createPortal(
+//     <div className="backdrop" onClick={props.onClick}></div>,
+//     document.getElementById("backdrop-hook"),
+//   );
+// };
+
+// export default Backdrop;
---
+++ src/components/SideDrawer.tsx
@@ -0,0 +1,23 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// // import Typography from "./../Typography/Typography";
+
+// interface SideDrawerProps {
+//   text?: any;
+//   type?: string;
+//   cssClass?: string;
+// }
+// const SideDrawer: React.FC<SideDrawerProps> = (props) => {
+ 
+//   const content = (
+//     <aside
+//       className={props.show ? "side-drawer show-nav" : "side-drawer"}
+//       onClick={props.onClick}
+//     >
+//       {props.children}
+//     </aside>
+//   );
+//   return ReactDOM.createPortal(content, document.getElementById("drawer-hook"));
+// };
+
+// export default SideDrawer;
